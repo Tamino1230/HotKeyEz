58e73ceb893d3b8b74399108aea9e90266228cf7
Tamino <roidingertamino@gmail.com>
2024-11-21 09:47:27 +0100

    Update click and extract me.bat

--- click and extract me.bat
+++ click and extract me.bat
@@ -1,7 +1,21 @@
 @echo off
+echo Searching for Python...
+for %%i in (python.exe) do set PYTHON_PATH=%%~$PATH:i
+
+REM finding...
+if "%PYTHON_PATH%"=="" (
+    echo Python wurde nicht gefunden. Bitte stellen Sie sicher, dass Python installiert ist und im PATH liegt.
+    exit /b 1
+)
+
 echo Installing Python packages...
 pip install keyboard
 echo Install successfull
 
-timeout 3
-start main.py
+echo looking for path...
+set SCRIPT_DIR=%~dp0
+
+echo start...
+cd /d %SCRIPT_DIR%
+%PYTHON_PATH% main.py
+pause
